7e291f9623f2765177452ac463a138041c34894f
Jannik Buhr <jannik.m.buhr@gmail.com>
2019-11-29 13:10:42 +0100

    initial

---
+++ awesome_app/app.R
@@ -0,0 +1,66 @@
+library(shiny)
+library(tidyverse)
+
+theme_set(theme_minimal())
+
+mismanaged_vs_gdp <- read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-05-21/per-capita-mismanaged-plastic-waste-vs-gdp-per-capita.csv") %>% 
+    janitor::clean_names() %>%
+    filter(!is.na(per_capita_mismanaged_plastic_waste_kilograms_per_person_per_day)) %>% 
+    rename(
+        gdp = gdp_per_capita_ppp_constant_2011_international_rate,
+        mismanaged = per_capita_mismanaged_plastic_waste_kilograms_per_person_per_day,
+        pop = total_population_gapminder
+    )
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+    
+    # Application title
+    titlePanel("Old Faithful Geyser Data"),
+    
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            selectizeInput(
+                inputId = "countries",
+                label = "Select some countries",
+                choices = mismanaged_vs_gdp$entity,
+                multiple = TRUE
+            )
+        ),
+        
+        # Show a plot of the generated distribution
+        mainPanel(
+            plotOutput("misPlt")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+    
+    output$misPlt <- renderPlot({
+        plt <- mismanaged_vs_gdp %>% 
+            ggplot(aes(gdp,
+                       mismanaged,
+                       label = entity)) +
+            geom_point(aes(size = pop)) +
+            scale_x_log10(labels = scales::dollar_format()) +
+            scale_y_log10(labels = scales::percent_format()) +
+            labs(x = "GDP per capita",
+                 y = "Percent mismanaged waste per capita",
+                 subtitle = "Size represents total population",
+                 title = "Test",
+                 caption = "test") +
+            guides(size = "none")
+        
+        label_data <- mismanaged_vs_gdp %>% 
+            filter(entity %in% input$countries)
+        
+        plt +
+            geom_text(data = label_data)
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
